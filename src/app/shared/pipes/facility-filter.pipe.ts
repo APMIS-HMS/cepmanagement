afd8e598b6fc57815025f522b3f3c1e7057f370e
kemijibzz <oluwakemi.awosile@apmis.ng>
2018-07-24 16:51:21 +0100

    Filter pipe returns searched string

--- src/app/shared/pipes/facility-filter.pipe.ts
+++ src/app/shared/pipes/facility-filter.pipe.ts
@@ -8,9 +8,13 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core';
 export class FacilityFilterPipe implements PipeTransform {
 
   transform(items: any[], searchText: string): any[] {
+  
     if(!items) return [];
     if(!searchText) return items;
-      //searchText = searchText.toString().toUpperCase(); 
-      return items.filter(item => item.name.indexOf(searchText) !== -1);
+
+      searchText = searchText.toLowerCase();
+      return items.filter( it => {
+        return it.name.toLowerCase().includes(searchText);
+      });
   }  
 }
